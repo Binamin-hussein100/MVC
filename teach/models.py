7d4eaefa70670e84ba24b5f478974a3ee6491885
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-02-25 14:41:19 +0300

    defined to-do model

--- teach/models.py
+++ teach/models.py
@@ -1,4 +1,17 @@
+from statistics import mode
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
 
+class ToDo(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+
+    class Meta:
+        verbose_name = ('ToDo')
+        verbose_name_plural  = ("Todo")
+    
+    def __str__(self):
+        return self.name
