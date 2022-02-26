e79adf35ab35d34eefdb87bf3d365119c5c24a8c
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-02-26 17:17:03 +0300

    added update form to view

--- teach/views.py
+++ teach/views.py
@@ -1,10 +1,20 @@
 import imp
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
+from .forms import *
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     my_todo = Tenants.objects.all()
+    if request.method == 'POST':
+        tenant_form = AddTenant(request.POST)
+        if tenant_form.is_valid():
+            print('tenant successfully saved')
+            messages.add_message(request,'Tenant successfully saved.')
+            return redirect('home')
+    else:
+        tenant_form = AddTenant(request.POST)
     return render(request,'homepage.html',{'my_todo':my_todo})
 
 
